9eb37c94a22ee9d03138c5293c141948a3844c49
hollylux <hollylux@gmail.com>
2017-06-08 18:04:19 -0400

    changing title

--- helloAngular/src/app/app.component.ts
+++ helloAngular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hello Angular from Bruce Lu 06/08/17 ';
 }
